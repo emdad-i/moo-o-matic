b0894a076c0b1b6a7f253edbe136a154a6fe2c51
emdad-digital <e.islam@outlook.com>
2018-08-28 09:50:53 +0100

    changed to allow return key to be detected on raspberry PI, tests agaisnt empty input

--- cow.py
+++ cow.py
@@ -1,11 +1,11 @@
-import keyboard #Using module keyboard
 from pygame import mixer
 import time
 
-while True:#making a loop
-	if keyboard.is_pressed('enter'):
-		mixer.init()
-		mixer.music.load('youtubecow.mp3')
-		mixer.music.play()
-		print('Booyaa! Ticket Done')
-		time.sleep(1) 
+while True:
+    text = raw_input("Hit return to moo")
+    if text == "":
+        mixer.init()
+    	mixer.music.load('youtubecow.mp3')
+    	mixer.music.play()
+    	print('Booyaa! Ticket Done')
+    	time.sleep(1)
